99a91dc03b2970881bf22671576e5e3597d13fac
Tom <tom@vistarmedia.com>
2013-07-29 13:34:56 -0400

    More UI updates

--- js/index.js
+++ js/index.js
@@ -1,28 +1,40 @@
-function addChart(album) {
+function addChart(album, showOverall) {
   $.getJSON("http://jaws.tomfleischer.com:5001/" + album).done(function(result) {
     data = []
     result.tracks.forEach(function(track) {
-      data.push([track.num, track.rel_pop, track.name])
+      if (showOverall) {
+        row = [track.num, track.rel_pop, track.name, track.pop, track.name]
+      } else {
+        row = [track.num, track.rel_pop, track.name]
+      }
+      data.push(row)
     });
     var dataTable = new google.visualization.DataTable();
     dataTable.addColumn('number', 'Track');
-    dataTable.addColumn('number', 'Popularity');
+    dataTable.addColumn('number', 'Relative');
     dataTable.addColumn({type: 'string', role: 'tooltip'});
+    if (showOverall) {
+      dataTable.addColumn('number', 'Overall');
+      dataTable.addColumn({type: 'string', role: 'tooltip'});
+    }
     dataTable.addRows(data);
 
     $el = $('<div>')
-    $el.append($('h3').val('Album Track Popularity'));
     $('.charts').append($el);
     var chart = new google.visualization.AreaChart($el[0]);
     chart.draw(dataTable, {
       title: '\'' + result.name + '\'' + ' by ' + result.artist,
-      titleTextStyle: { fontSize: 18, color: '#E3D5B6' },
+      titleTextStyle: { fontSize: 16, color: '#E3D5B6' },
       areaOpacity: '0.6',
+      lineWidth: 0,
+      pointSize: 3,
+      colors: ['#3497C2', '#DBAB39'],
       backgroundColor: '#333',
       fontName: 'Raleway',
       chartArea: {
-        top: 40,
-        left: 0
+        top:    40,
+        left:   0,
+        height: 80
       },
       legend: {
         position: 'none'
@@ -35,7 +47,8 @@ function addChart(album) {
       vAxis: {
         textPosition: 'none',
         baselineColor: 'transparent',
-        gridlines: { color: 'transparent' }
+        gridlines: { color: 'transparent' },
+        viewWindow: { max: 1, min: 0 }
       }
     });
   });
